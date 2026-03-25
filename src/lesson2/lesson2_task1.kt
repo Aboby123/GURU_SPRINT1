b028426d41bc81c4b756fdad8f026bd26678574f
Влад <pepepe228337@gmail.com>
2026-03-25 14:39:20 +0300

    GS-2-1 completed 6

--- src/lesson2/lesson2_task1.kt
+++ src/lesson2/lesson2_task1.kt
@@ -4,6 +4,11 @@ fun main() {
     val ageOfMajority = 19
     println("Введите ваш возраст:")
     val userAge = readln().toInt()
-    val isAdult = userAge >= ageOfMajority
-    println("Совершеннолетие достигнуто: $isAdult")
+    val message = if (userAge >= ageOfMajority) {
+        "добро пожаловать"
+    } else {
+        "вход запрещен, подрастите еще"
+    }
+    println(message)
+
 }
