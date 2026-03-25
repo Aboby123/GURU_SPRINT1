e11098f53687c4b166a36c19cd2113276d5b4976
Влад <pepepe228337@gmail.com>
2026-03-25 14:32:54 +0300

    GS-2-1 completed 6

--- src/lesson2/lesson2_task1.kt
+++ src/lesson2/lesson2_task1.kt
@@ -1,1 +1,9 @@
 package lesson2
+
+fun main() {
+    val ageOfMajority = 19
+    println("Введите ваш возраст:")
+    val userAge = readln().toInt()
+    val isAdult = userAge >= ageOfMajority
+    println("Совершеннолетие достигнуто: $isAdult")
+}
